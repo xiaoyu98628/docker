47330b708df1a7dc848a4546fedac0f770dfa6ce
xiaoyu <1765841705@qq.com>
2025-07-20 21:02:25 +0800

    update: mysql

--- mysql/8/init/entrypoint.sh
+++ mysql/8/init/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-envsubst < /docker-entrypoint-initdb.d/init.template.sql > /docker-entrypoint-initdb.d/init.sql
+envsubst < /docker-entrypoint-initdb.d/init.sql.template. > /docker-entrypoint-initdb.d/init.sql
--- mysql/8/init/init.sql
+++
@@ -1,3 +0,0 @@
-GRANT ALL ON *.* TO 'admin'@'%';
--- 刷新权限
-FLUSH PRIVILEGES;
